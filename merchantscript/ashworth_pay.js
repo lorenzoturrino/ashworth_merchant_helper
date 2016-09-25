522bf67718f5e6427a91ae8c693aea3b2c30080b
lorenzoturrino <lorenzo.turrino@gmail.com>
2016-09-25 12:59:36 +0100

    add sample merchant page html

--- merchantscript/ashworth_pay.js
+++ merchantscript/ashworth_pay.js
@@ -1,2 +1,13 @@
-// require jquery for the mvp scope. might go full js for lightweight or react for fancy
+// require jquery for the mvp scope. might go full js for lightweight or react for fancy.
+// Add jquery and this script to your page, add id="ashworthpay" to the element where you want the payment form.
 
+$( document ).ready(function() {
+
+    hook = $('.ashworthpay');
+    if(!hook.length) {
+        console.log('WARNING! no element found to hook the payment form to.');
+    } else {
+        // stuff.
+    }
+
+});
