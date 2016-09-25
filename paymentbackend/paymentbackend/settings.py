6d290155b49d3c958d74bbfb7d2ae5c28300fe6f
lorenzoturrino <lorenzo.turrino@gmail.com>
2016-09-25 01:15:59 +0100

    add and hook api app

--- paymentbackend/paymentbackend/settings.py
+++ paymentbackend/paymentbackend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     #apps
     'storage',
+    'api',
 ]
 
 MIDDLEWARE = [
--- paymentbackend/paymentbackend/urls.py
+++ paymentbackend/paymentbackend/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^storage/', include('storage.urls'))
+    url(r'^storage/', include('storage.urls')),
+    url(r'^api/v1/', include('api.urls')),
 ]
