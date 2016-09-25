7a7e3fd755329dc0c530e1384484ff2b271b5696
lorenzoturrino <lorenzo.turrino@gmail.com>
2016-09-25 13:55:33 +0100

    post form stub

--- merchantscript/ashworth_pay.js
+++ merchantscript/ashworth_pay.js
@@ -3,18 +3,32 @@
 
 $( document ).ready(function() {
 
-    var form_html = '<form id="ashworth-pay-form"><input type="number" name="cardnumber" placeholder="card number">' +
+    formSetup();
+
+    function formSetup() {
+        var form_html = '<form id="ashworth-pay-form"><input type="number" name="cardnumber" placeholder="card number" id="card-number">' +
         '<input type="number" name="month" placeholder="expiry month">' +
         '<input type="number" name="year" placeholder="expiry year">' +
         '<input type="number" name="ccv" placeholder="card ccv"></form>' +
-        '<input type="submit"></form>'
-        
-    var hook = $('#ashworthpay');
-    
-    if(!hook.length) {
-        console.log('WARNING! no element found to hook the payment form to.');
-    } else {
-        hook.append(form_html)
+        '<input type="submit" id="ashworth-send"></form>'
+
+        var hook = $('#ashworthpay');
+
+        if(!hook.length) {
+            console.log('WARNING! no element found to hook the payment form to.');
+        } else {
+            hook.append(form_html);
+            $('#ashworth-send').click(sendData);
+        }
     }
+
+    function sendData(){
+        var cardNumber = $("#card-number");
+        var value = 73.74;
+        var currency = 'GBP';
+
+        $.post();
+    }
+
 
 });
