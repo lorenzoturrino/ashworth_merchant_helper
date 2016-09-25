ca7c9b2028e825fddfc1194c8993810b779e73a6
lorenzoturrino <lorenzo.turrino@gmail.com>
2016-09-25 01:54:21 +0100

    add csfr exemption, more view error handling for missing parameters

--- paymentbackend/api/views.py
+++ paymentbackend/api/views.py
@@ -1,16 +1,20 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse, HttpResponse
 from storage.models import Processor
 
-def best_quote(request):
+@csrf_exempt
+def best_quote(request):  # returns the best payment method. Expects a transaction_value:value key pair via POST
     if request.method == 'POST':
-        min_commission = float('inf')
-        best_method = None
-        for psp in Processor.objects.all():
-            if psp.transaction_cost(20) < min_commission:
-                best_method = psp.name
-        return JsonResponse({'suggested_method':best_method})
-    else:
-        return HttpResponse("This endpoint only accepts POST requests", status=405)
+        if request.POST.get('transaction_value'):
+            min_commission = float('inf')
+            best_method = None
+            for psp in Processor.objects.all():
+                if psp.transaction_cost(20) < min_commission:
+                    best_method = psp.name
+            return JsonResponse({'suggested_method':best_method})
+        return HttpResponse('Missing required parameter', status=400)
+    return HttpResponse('This endpoint only accepts POST requests', status=405)
 
-def index(request):
+
+def index(request):  # just a landing page
     return HttpResponse('This is the Ashworth quoting service v1 API. Have fun.', status=200)
