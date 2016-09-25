10dbc2439469ab598ef419fda36e5209d65abd1a
lorenzoturrino <lorenzo.turrino@gmail.com>
2016-09-25 02:06:27 +0100

    transaction admin and namechange in model

--- paymentbackend/logger/admin.py
+++ paymentbackend/logger/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Transaction
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    readonly_fields = ['amount', 'method', 'transaction_fee', 'time']
+    list_display = ['amount', 'method', 'transaction_fee', 'time']
--- paymentbackend/logger/models.py
+++ paymentbackend/logger/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Transaction(models.Model):
     amount = models.FloatField()
     method = models.CharField(max_length=255)
-    commission = models.FloatField()
+    transaction_fee = models.FloatField()
     time = models.DateTimeField(auto_now=True)
