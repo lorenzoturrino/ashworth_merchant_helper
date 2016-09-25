a063450feb635e8aec5905d923ad774513550901
lorenzoturrino <lorenzo.turrino@gmail.com>
2016-09-25 14:15:31 +0100

    switch to cors middleware, since the old one is not maintained in and django 1.10 incompatible

--- paymentbackend/paymentbackend/settings.py
+++ paymentbackend/paymentbackend/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,7 +124,11 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_METHODS = (
+    'GET',
+    'POST',
+)
