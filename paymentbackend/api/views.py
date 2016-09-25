8eadf1e36fed1dae6e69e499c269d550175d2e90
lorenzoturrino <lorenzo.turrino@gmail.com>
2016-09-25 01:59:04 +0100

    fix logic error i totally didnt write

--- paymentbackend/api/views.py
+++ paymentbackend/api/views.py
@@ -2,15 +2,18 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse, HttpResponse
 from storage.models import Processor
 
+
 @csrf_exempt
 def best_quote(request):  # returns the best payment method. Expects a transaction_value:value key pair via POST
     if request.method == 'POST':
-        if request.POST.get('transaction_value'):
+        transaction_value = float(request.POST.get('transaction_value'))
+        if transaction_value:
             min_commission = float('inf')
             best_method = None
             for psp in Processor.objects.all():
-                if psp.transaction_cost(20) < min_commission:
+                if psp.transaction_cost(transaction_value) < min_commission:
                     best_method = psp.name
+                    min_commission = psp.transaction_cost(transaction_value)
             return JsonResponse({'suggested_method':best_method})
         return HttpResponse('Missing required parameter', status=400)
     return HttpResponse('This endpoint only accepts POST requests', status=405)
