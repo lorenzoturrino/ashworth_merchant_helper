2a31cc51a0c53ed19022a1510d5ae0134d5e3bb0
lorenzoturrino <lorenzo.turrino@gmail.com>
2016-09-25 01:29:35 +0100

    more view stuff

--- paymentbackend/api/urls.py
+++ paymentbackend/api/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'api'
 urlpatterns = [
+    url(r'^best_quote/$', views.best_quote, name='best_quote'),
     url(r'^$', views.index, name='index'),
 ]
--- paymentbackend/api/views.py
+++ paymentbackend/api/views.py
@@ -1,8 +1,11 @@
 from django.http import JsonResponse, HttpResponse
 
 
-def quote(request):
+def best_quote(request):
     if request.method == 'POST':
         return JsonResponse({'foo':'bar'})
     else:
         return HttpResponse(status=405)
+
+def index(request):
+    return HttpResponse('This is the Ashworth quoting service v1 API. Have fun.')
